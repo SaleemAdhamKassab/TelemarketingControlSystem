a361899b876bd026bc8e5d03a4f416935829fb04
Mhd Ismail <Mhd Ismail@mhdismail725>
2024-04-16 15:37:51 +0300

    Change Connection string to remote

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddSwaggerGen();
 
 // Sql server Db Connections
 //builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("RemoteDb")));
 
 
 //services
