9b1c29240076087544a42d488800a9ed3e6e46c4
Saleem Kassab <saleemkassab5@gmail.com>
2024-08-04 13:13:50 +0300

    add Is Active

---
+++ Migrations/20240804101322_Test.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TelemarketingControlSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class Test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Employees",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Employees");
+        }
+    }
+}
